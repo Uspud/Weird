16dbb60281793542cac7c0c3330da2240dd0302b
Uspud <symsool@naver.com>
2022-01-23 20:05:09 +0900

    2022-01-23 20:11

---
+++ BOJ/C++/Greedy_Algorithm/13305.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);   // 입출력 빠르게 하기
+
+using namespace std;
+
+int main(void) {
+    
+}
--- BOJ/C++/Greedy_Algorithm/1541.cpp
+++ BOJ/C++/Greedy_Algorithm/1541.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
 #define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);   // 입출력 빠르게 하기
 
 using namespace std;
+
+bool flag;
+int ans;
 
 int main(void) {
     fastio;
@@ -13,38 +15,29 @@ int main(void) {
 
     int length_s = s.size();
 
-    vector<int> var, sym; int start = 0;
+    vector<int> var; int start = 0;
 
     for(int i = 0; i <= length_s; i++) {
-        if(s[i] == '+' || s[i] == '-' ) {
-            char temp[i - start];
-            for(int j = 0; j < i - start; j++) {
+        if(s[i] == '+' || s[i] == '-' || i == length_s) {
+            char temp[i - start+1];
+            for(int j = 0; j <= i - start; j++) {
                 temp[j] = s[start + j];
             }
-            var.push_back(stoi(temp));
-            sym.push_back(int(s[i]));
+            temp[i-start] = '\0';
+            if(flag)    var.push_back(-1 * stoi(temp));
+            else        var.push_back(stoi(temp));
             start = i + 1;
-        }
-        else if(i == length_s) {
-            char temp[i - start];
-            for(int j = 0; j < i - start; j++) {
-                temp[j] = s[start + j];
-            }
-            var.push_back(stoi(temp));
+            if(s[i] == '-' && !flag) flag = !flag;
         }
     }
 
-    int var_size = var.size(), sym_size = sym.size();
+    int var_size = var.size();
 
-    for(int i = 0; i < var_size; i++) {
-        cout << var[i] << ' ';
-    }
+    // for(int i = 0; i < var_size; i++)   cout << var[i] << ' ';
+    
+    for(int i = 0; i < var_size; i++)   ans += var[i];
 
-    cout << '\n';
-
-    for(int i = 0; i < sym_size; i++) {
-        cout << char(sym[i]) << ' ';
-    }
+    cout << ans;
 
     return 0;
 }
